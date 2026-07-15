EhPanda-Team/EhPanda

fix: fixed thumbnail loading issues when certain thumbnail configurations are used

EhPanda/View/Detail/Previews/PreviewsView.swift
@@ -55,7 +55,7 @@ struct PreviewsView: View {
                     }
                     .onAppear {
                         if store.databaseLoadingState != .loading
-                            && store.previewURLs[index] == nil && (index - 1) % 20 == 0
+                            && store.previewURLs[index] == nil && (index - 1) % 10 == 0
                         {
                             store.send(.fetchPreviewURLs(index))
                         }

12dora/LegacyEhPanda

(cherry picked from commit d98965fa711e372a43fe7bcaced7a098e1aee5bd)

EhPanda/View/Detail/Previews/PreviewsView.swift
@@ -57,7 +57,7 @@ struct PreviewsView: View {
                     }
                     .onAppear {
                         if viewStore.databaseLoadingState != .loading
-                            && viewStore.previewURLs[index] == nil && (index - 1) % 20 == 0
+                            && viewStore.previewURLs[index] == nil && (index - 1) % 10 == 0
                         {
                             viewStore.send(.fetchPreviewURLs(index))
                         }